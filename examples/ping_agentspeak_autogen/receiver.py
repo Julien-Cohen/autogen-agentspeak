bd18b26caa1c6fa0e4c568d68061bcbe2722349d
juliencohen <Julien.Cohen@univ-nantes.fr>
2025-09-08 16:52:09 +0200

    Clean.

--- examples/ping_agentspeak_autogen/receiver.py
+++ examples/ping_agentspeak_autogen/receiver.py
@@ -1,22 +1,13 @@
-import os
 
-
-import agentspeak.runtime
-import agentspeak.stdlib
-from autogen_core import RoutedAgent, type_subscription, message_handler, MessageContext
-
-
-import message as message_module
+from autogen_core import type_subscription, message_handler, MessageContext
 
 import agentspeak_autogen.bdi
-
 
 @type_subscription(topic_type="to_receiver")
 class ReceiverAgent(agentspeak_autogen.bdi.BDIAgent):
 
     def __init__(self, descr):
         super().__init__(descr, "receiver.asl")
-
 
     @message_handler
     async def handle_message(self, message: agentspeak_autogen.bdi.MyMessage, ctx: MessageContext) -> None:
--- examples/ping_agentspeak_autogen/sender.py
+++ examples/ping_agentspeak_autogen/sender.py
@@ -1,22 +1,15 @@
-import os
 
-import agentspeak.runtime
-import agentspeak.stdlib as agentspeak_stdlib
-from autogen_core import RoutedAgent, type_subscription, message_handler, MessageContext, TopicId
+from autogen_core import  type_subscription, message_handler, MessageContext, TopicId
 
 import message as message_module
 
-import asyncio
-
 import agentspeak_autogen.bdi
-
 
 @type_subscription(topic_type=message_module.asp_message_send)
 class SenderAgent(agentspeak_autogen.bdi.BDIAgent):
 
     def __init__(self, descr):
         super().__init__(descr, "sender.asl")
-
 
     @message_handler
     async def handle_message(self, message: agentspeak_autogen.bdi.MyMessage, ctx: MessageContext) -> None:
